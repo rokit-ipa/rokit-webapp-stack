3ff972770f67df3c78456168b8ad2d906d3fb7fc
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2023-07-28 10:09:56 +0200

    added nicegui module to /main endpoint

--- view/rokitAPI/main.py
+++ view/rokitAPI/main.py
@@ -2,6 +2,7 @@
 import uvicorn
 from fastapi import FastAPI
 from database import read_influxdb
+from view import frontend
 
 app = FastAPI()
 results = read_influxdb.read()
@@ -10,5 +11,7 @@ results = read_influxdb.read()
 def read_root():
     return {"message": "OK"}
 
+frontend.init(app)
+
 if __name__ == '__main__':
-    uvicorn.run(f"{Path(__file__).stem}:app", host="localhost", port=8000, reload=True)
+    uvicorn.run(f"{Path(__file__).stem}:app", host="localhost", port=8000, reload=True) 

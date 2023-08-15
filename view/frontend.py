f5776665193c945ab2a43b4da9eb70e1cc95c103
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2023-08-15 19:37:09 +0200

    revamped the UI with new design and elements

--- view/frontend.py
+++ view/frontend.py
@@ -1,69 +1,78 @@
 from fastapi import FastAPI
-from nicegui import app, ui
-import matplotlib
-import numpy
+from nicegui import ui
 
-from database import read_influxdb
-
-TABS = ["Run a Test", "View and Edit the Test", "Testcases Protocol Guidelines"]
-TESTCASES_LIST = ["MAX_VELOCITY", "MAX_VELOCITY_SLOPE", "BREAKING_DISTANCE"]
+# Define the constants
+conditions_file_path = "view/docs/conditions.md"
+protocols_file_path = "view/docs/protocols.md"
+TABS = ["Run Tests", "Results", "Test Protocols", "Test Conditions"]
+INPUTS_PER_TAB = 3
+INPUT_NAMES = ["Temperature (C)", "Humidity (%)", "Inclination(degrees)"]
+DROPDOWN_OPTIONS = ["Option 1", "Option 2", "Option 3"]
+TESTCASES_LIST = ["MAX_VELOCITY", "MAX_VELOCITY_SLOPE"]
 TRACKER_OBJECT_LIST = ["rokit_1", "rokit_2"]
 
+
 def init(fastapi_app: FastAPI) -> None:
-    
-    def get_db_readings(): 
-        results = read_influxdb.read()
-        return results; 
-    
-    def set_test_parameters():
-        print("here")
 
+    def read_markdown_file(file_path):
+        with open(file_path, "r") as f:
+            markdown_content = f.read()
+        return markdown_content
 
-    @ui.page('/main')
-    def show():
+    def set_background(color: str) -> None:
+        ui.query('body').style(f'background-color: {color}')
 
-        ui.label('Rokit Testing Application').classes('text-h4')
+    @ui.page('/ui')
+    def view():
+        # ui.dark_mode().bind_value(app.storage.user, 'dark_mode')
+        # ui.checkbox('dark mode').bind_value(app.storage.user, 'dark_mode')
+        with ui.header().classes(replace='row items-center') as header:
+            with ui.tabs().classes('w-full justify-center') as tabs:
+                for tab in TABS:
+                    ui.tab(tab)
 
-        with ui.tabs() as tabs:
-            for tab in TABS: 
-                ui.tab(tab)
+        with ui.footer(value=True) as footer:
+            ui.label(
+                'RoKit testing stack @ Fraunhofer IPA 2023').classes('absolute-center items-center')
 
-        with ui.footer(value=False) as footer:
-            ui.label('Footer')
+        input_values = {}
+        with ui.tab_panels(tabs):
+            with ui.tab_panel(TABS[0]):
+                ui.label('Configure the Test').classes('text-h4')
+                with ui.card():
+                    for input_name in INPUT_NAMES:
+                        value = ui.input(input_name)
+                        # this value has to be pushed to the database
+                        input_values[input_name] = value
 
+                with ui.card():
+                    with ui.row():
+                        ui.button('Save', on_click=lambda: ui.notify(
+                            'Yay!', type='positive'))
+                        ui.button('Start', on_click=lambda: ui.colors(
+                            primary='#2bcf61'))
+                        ui.button('Stop', on_click=lambda: ui.colors())
+                with ui.left_drawer(value=True).classes('bg-blue-100') as left_drawer_0:
+                    ui.label('Select Test')
+                    with ui.element('div').classes('p-2 bg-blue-100'):
+                        select0 = ui.select(
+                            TESTCASES_LIST, value=TESTCASES_LIST[0])
+                        print(str(select0))
 
-        with ui.page_sticky(position='bottom-right', x_offset=20, y_offset=20):
-            ui.button(on_click=footer.toggle, icon='contact_support').props('fab')
-
-        with ui.tab_panels(tabs, value=TABS[0]).classes('w-1/5'):
-            with ui.tab_panel('Run a Test'):
-                ui.label('Testcase Configuration').classes('text-h5')
-                ui.element('div').classes('p-2 bg-white-100')
-                ui.label('Choose a testcase...')
-                with ui.element('div').classes('p-2 bg-blue-100'):
-                    select1 = ui.select(TESTCASES_LIST, value=TESTCASES_LIST[0])
-                ui.label('Choose the tracking object...')
-                radio1 = ui.radio(TRACKER_OBJECT_LIST, value=TRACKER_OBJECT_LIST[0]).props('inline')
-                ui.label('Test Protocol Notes...')
-                ui.textarea(label='Text', placeholder='start typing',
-                    on_change=lambda e: result.set_text('you typed: ' + e.value))
-                result = ui.label()
-                
             with ui.tab_panel(TABS[1]):
-                print(get_db_readings())
-                data = get_db_readings()
-                with ui.pyplot(figsize=(3, 2)):
-                    print(data)
-                    x = numpy.linspace(data[0][1], data[1][1], data[2][1])
-                    y = numpy.log(x)
-                    matplotlib.pyplot.title('Velocity Graph')
-                    matplotlib.pyplot.plot(x, y, '-')
+                ui.label('Test Results').classes('text-h4')
+
             with ui.tab_panel(TABS[2]):
-                ui.label('')
+                markdown_content = read_markdown_file(protocols_file_path)
+                ui.markdown(markdown_content)
 
-
+            with ui.tab_panel(TABS[3]):
+                markdown_content = read_markdown_file(conditions_file_path)
+                ui.markdown(markdown_content)
 
     ui.run_with(
         fastapi_app,
-        storage_secret='pick your private secret here',  # NOTE setting a secret is optional but allows for persistent storage per user
+        # NOTE setting a secret is optional but allows for persistent storage per user
+        storage_secret='pick your private secret here',
+        title='Robot Testing Kit'
     )

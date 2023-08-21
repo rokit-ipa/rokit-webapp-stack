4e0e65e03e0f92ffab7f5491e7f4523771df9149
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2023-08-21 10:27:16 +0200

    modified the Models, all basic functions are implemented

--- rokit_app/frontend.py
+++ rokit_app/frontend.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI
-import json
-import requests
 import random
 from nicegui import ui
 from nicegui.events import ValueChangeEventArguments
+from rokit_app.rokitAPI import main as api
+from rokit_app.rokitAPI.models import TestParameters, TestResults
+
+
 
 # Define the constants
-API_URL = "http://localhost:8001"
 conditions_file_path = "rokit_app/static/conditions.md"
 protocols_file_path = "rokit_app/static/protocols.md"
 TABS = ["Run Tests", "Results", "Test Protocols", "Test Conditions"]
-INPUTS_PER_TAB = 5
-INPUT_NAMES = ["Temperature", "Humidity",
-               "Inclination", "Floor-type", "Notes"]
+ID_INPUT = ["trial_number"]
+FLOAT_INPUT = ["temperature", "humidity", "inclination", ]
+STR_INPUT = ["floor_type", "notes"]
 TESTCASES = ["MAX_VELOCITY", "MAX_VELOCITY_SLOPE"]
 
 
@@ -23,19 +24,26 @@ def init(fastapi_app: FastAPI) -> None:
             markdown_content = f.read()
         return markdown_content
 
-    def save_parameters(payload):
-        response = requests.post(f"{API_URL}/setparams", json=payload)
-        print(response.status_code)
-        print(response.json())
-
-    def start_test(trial=1):
-        response = requests.post(f"{API_URL}/getresults", json=payload)
-        print(response.status_code)
-        print(response.json())
-
     def show(event: ValueChangeEventArguments):
         name = type(event.sender).__name__
         ui.notify(f'{name}: {event.value}')
+
+    def submit_params(payload):
+        # request = TestParameters(**payload)
+        # api.set_params(request)
+        request = TestParameters(
+            test_name=payload["test_name"],
+            trial_number=payload["trial_number"],
+            tracking_object=payload["tracking_object"],
+            temperature=payload["temperature"],
+            humidity=payload["humidity"],
+            inclination=payload["inclination"],
+            floor_type=payload["floor_type"],
+            notes=payload["notes"],
+        )
+        api.set_params(request)
+
+
 
     @ui.page('/ui')
     def view():
@@ -55,7 +63,7 @@ def init(fastapi_app: FastAPI) -> None:
             ui.label('')
         
 
-        input_values = {}
+
         with ui.tab_panels(tabs, value=TABS[0]):
             with ui.tab_panel(TABS[1]):
                 #ui.label('Test Results').classes('text-h4 w-full')
@@ -66,6 +74,7 @@ def init(fastapi_app: FastAPI) -> None:
                                      'humidity': 75,
                                      'inclination': 5,
                                      'floortype': 'wood',
+                                     'tracking_object': 'rokit_1',
                                      'notes': 'ambient light was less',
                                      'velocity': random.uniform(5, 8), })
                     table2.add_rows({'trial': item,
@@ -73,6 +82,7 @@ def init(fastapi_app: FastAPI) -> None:
                                      'humidity': 75,
                                      'inclination': 5,
                                      'floortype': 'wood',
+                                     'tracking_object': 'rokit_2',
                                      'notes': 'ambient light was less',
                                      'velocity': random.uniform(5, 8), })
                 columns = [
@@ -83,10 +93,10 @@ def init(fastapi_app: FastAPI) -> None:
                         'label': 'Humidity(%)', 'field': 'humidity'},
                     {'name': 'inclination',
                         'label': 'Inclination(degree)', 'field': 'inclination'},
+                    {'name': 'tracking_object', 'label': 'Tracking Object', 'field': 'tracking_object'},
                     {'name': 'floortype', 'label': 'Floor type', 'field': 'floortype'},
                     {'name': 'notes', 'label': 'Notes', 'field': 'notes'},
-                    {'name': 'velocity',
-                        'label': 'Velocity(m/s)', 'field': 'velocity'},
+                    {'name': 'velocity','label': 'Velocity(m/s)', 'field': 'velocity'},
                 ]
 
                 ui.label('MAX_VELOCITY Test Results').classes('text-h4')
@@ -97,47 +107,68 @@ def init(fastapi_app: FastAPI) -> None:
                 table2 = ui.table(columns=columns, rows=[],
                                   row_key='trial').classes('w-full my-10')
 
-            with ui.tab_panel(TABS[0]):
+            with ui.tab_panel(TABS[0]):                
+                payload = {
+                    "test_name": "MAX_VELOCITY",    
+                    "trial_number": 1,         
+                    "tracking_object": "none",     
+                    "temperature": 0.0,        
+                    "humidity": 0.0,           
+                    "inclination": 0.0,        
+                    "floor_type": "none",          
+                    "notes": "none",               
+                }
+
+                def update_payload(name, value):
+                    payload[name] = value
+
                 ui.label('Configure the Test').classes('text-h4')
 
                 with ui.row().classes('items-center'):
                     with ui.card().classes('my-5 h-40'):
                         ui.label('Select Test').classes('text-h6')
-                        with ui.element('div').classes('p-2 bg-blue-100'):
-                            select0 = ui.select(
-                                TESTCASES, value=TESTCASES[0])
-                            print(str(select0))
+                        with ui.element('div').classes('p-3 bg-blue-100'):
+                            select0 = ui.select(TESTCASES, value=TESTCASES[0])
+                            update_payload("test_name", select0.value)
                     with ui.card().classes('my-0 h-40'):
                         ui.label('Select the tracking object').classes(
                             'text-h6')
-                        ui.radio(['tracker_1', 'tracker_2'],
-                                 value='A', on_change=show).props('inline')
+                        ui.radio(['tracker_1', 'tracker_2'],value='tracker_1', on_change=lambda e: update_payload("tracking_object", str(e.value))).props('inline')
 
                     with ui.card().classes('my-0 h-120'):
                         ui.label('Set environment conditions').classes(
                             'text-h6')
-                        for input_name in INPUT_NAMES:
-                            value = ui.input(input_name)
-                            input_values[input_name] = value
+                        for field_name in ID_INPUT:
+                            ui.input(field_name, on_change=lambda e: update_payload(field_name, str(e.value)))
 
-                payload = {
-                    "test_name": "Test Case 1",
-                    "temperature": 25.5,
-                    "humidity": 60.2,
-                    "inclination": 5.7,
-                    "floor_type": "Wooden",
-                    "notes": "Sample notes"
-                }
-     # on_click=lambda: ui.notify('Saved parameters!', type='positive',
+                        for field_name in FLOAT_INPUT:
+                            ui.input(field_name, on_change=lambda e: update_payload(field_name, float(e.value)))
+                            # value = ui.input(field_name)
+                            # payload[field_name] = value
+
+                        for field_name in STR_INPUT:
+                            ui.input(field_name, on_change=lambda e: update_payload(field_name, str(e.value)))
+
+                            
+                        
+                            # value = ui.input(field_name)
+                            # payload[field_name] = value
+
+                ## on_click=lambda: ui.notify('Saved parameters!', type='positive',
+
+                def update_trial(number):
+                    payload["trial_number"]=int(number)
+
                 with ui.card().classes('my-5'):
                     with ui.row().classes('items-center'):
-                        ui.button('Save parameters', on_click=lambda: save_parameters(payload)).classes('items-center')
-                        ui.button('Trial 1', on_click=add_row(1))
-                        ui.button('Trial 2', on_click=add_row(2))
-                        ui.button('Trial 3', on_click=add_row(3))
-                        ui.button('Trial 4', on_click=add_row(4))
-                        ui.button('Trial 5', on_click=add_row(5))
-                        ui.button('Completed', on_click=lambda: ui.colors())
+                        ui.button('Save parameters', on_click=submit_params(payload))
+                        ui.button('Start Test', on_click=update_trial(1))
+                        # ui.button('Trial 1', on_click=update_trial(1))
+                        # ui.button('Trial 2', on_click=update_trial(2))
+                        # ui.button('Trial 3', on_click=update_trial(3))
+                        # ui.button('Trial 4', on_click=update_trial(4))
+                        # ui.button('Trial 5', on_click=update_trial(5))
+                        ui.button('Test Result', on_click=lambda: ui.colors())
 
 
             with ui.tab_panel(TABS[2]):
--- rokit_app/rokitAPI/models.py
+++ rokit_app/rokitAPI/models.py
@@ -2,13 +2,15 @@ from pydantic import BaseModel
 
 
 class TestParameters(BaseModel):
-    test_name: str
-    temperature: float
-    humidity: float
-    inclination: float
-    floor_type: str
-    notes: str
+    test_name: str = ''
+    trial_number: int = 0
+    tracking_object: str = ''
+    temperature: float= 0.0
+    humidity: float=0.0
+    inclination: float=0.0
+    floor_type: str=''
+    notes: str=''
 
 class TestResults(BaseModel):
-    trial: int
-    velocity: float
+    trial: int=0
+    velocity: float=0.0

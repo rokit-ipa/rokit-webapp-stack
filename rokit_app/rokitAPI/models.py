5ca86de84b1677c45e57ac3b5be941b21c637535
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2023-08-22 13:30:55 +0200

    added robot_name field to the model and consolidated the UI for now

--- rokit_app/rokitAPI/models.py
+++ rokit_app/rokitAPI/models.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 class TestParameters(BaseModel):
     test_name: str = ''
     trial_number: int = 0
+    robot_name: str = ''
     tracking_object: str = ''
     temperature: float = 0.0
     humidity: float = 0.0
